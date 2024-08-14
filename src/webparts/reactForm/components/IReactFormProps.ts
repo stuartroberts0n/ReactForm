98afe21496006501e04116e8150c66920506569b
Stuart Robertson <Stuart.Robertson@guelph.ca>
2024-08-14 10:26:38 -0400

    initial commit, WebPart using React with @pnp/sp

---
+++ src/webparts/reactForm/components/IReactFormProps.ts
@@ -0,0 +1,7 @@
+export interface IReactFormProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/reactForm/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IReactFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ReactFormWebPartStrings' {
+  const strings: IReactFormWebPartStrings;
+  export = strings;
+}
